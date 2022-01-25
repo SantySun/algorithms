ccecb8749a2f6dcf39f267b13e6c24cf83d14094
Santy <sunjixiang.gz@gmail.com>
2022-01-26 09:54:48 +1100

    Add sort class

---
+++ sort.py
@@ -0,0 +1,29 @@
+from random import shuffle
+
+nums = [i for i in range(1, 101)]
+shuffle(nums)
+
+
+class Sort:
+    def __init__(self) -> None:
+        pass
+    
+    @classmethod
+    def bubble_sort(self, list):
+        pass
+    
+    @classmethod
+    def selection_sort(self, list):
+        pass
+
+    @classmethod
+    def quick_sort(self, list):
+        pass
+
+    @classmethod
+    def shell_sort(self, list):
+        pass
+
+    @classmethod
+    def merge_sort(self, list):
+        pass
